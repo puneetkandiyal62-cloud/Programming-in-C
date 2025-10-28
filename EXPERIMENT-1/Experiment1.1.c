d4219d1514012199aa71709b829785b36da7159e
puneetkandiyal62-cloud <puneetkandiyal62@gmail.com>
2025-10-28 15:48:46 +0530

    Add files via upload

---
+++ EXPERIMENT-1/Experiment1.1.c
@@ -0,0 +1,11 @@
+  /* Experiment 1: Installation, Environment Setup and starting with C language
+1. write a c program to print "Hello world"*/
+
+#include<stdio.h>
+int main()
+{
+	printf ("Puneet Kandiyal\nSAP ID: 590025456\nCourse- BSc CS\nBatch-1");  
+    printf("\n-------------------------------------------------------------\n");
+	printf("Hello world");
+	return 0;
+ } 
