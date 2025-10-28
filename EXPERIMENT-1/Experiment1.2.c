ba8ba8f335400b98c6b6557e44e3621e52ec9208
puneetkandiyal62-cloud <puneetkandiyal62@gmail.com>
2025-10-28 15:49:18 +0530

    Add files via upload

---
+++ EXPERIMENT-1/Experiment1.2.c
@@ -0,0 +1,12 @@
+/* Experiment 1: installation, environment setup and starting 
+    2. write a c program to print the address in multiple lines (new line).*/
+
+#include<stdio.h>
+int main()
+{
+	printf ("Puneet Kandiyal\nSAP ID: 590025456\nCourse- BSc CS\nBatch-1");  
+    printf("\n-----------------------------------------------------------------\n");
+	printf("Rishikesh \n14 bigha \nRishikedh\n349137");
+	return 0;
+ } 
+ 
