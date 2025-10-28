b0d77460fecd8c44fc3074eaa51fc97794d02020
puneetkandiyal62-cloud <puneetkandiyal62@gmail.com>
2025-10-28 16:19:46 +0530

    Add files via upload

---
+++ EXPERIMENT - 4/Experiment4.3.4.c
@@ -0,0 +1,20 @@
+/* 4.3 Patterns: 4*/
+#include<stdio.h>
+int main()
+{
+	printf ("Puneet Kandiyal\nSAP ID: 590025456\nCourse- BSc CS\nBatch-1");  
+    printf("\n-------------------------------------------------------------\n");
+	printf("The pattern is:\n");
+	int i, j;
+	for(i=1; i<=5; i++)
+	{
+		int k=i;
+		for(j=1; j<=i; j++)
+		{
+			printf("%d", k);
+			k--;
+		}
+		printf("\n");
+	}
+	return 0;
+}
